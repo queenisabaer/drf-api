c8af24d558244062e2a2fb5d29158e458a7d56cf
Dajana Isbaner <dajanaisbaner@gmx.de>
2024-07-24 12:02:24 +0000

    Add extra fields to post and profiles serializers

--- posts/serializers.py
+++ posts/serializers.py
@@ -1,11 +1,13 @@
 from rest_framework import serializers
 from .models import Post
+from likes.models import Like
 
 class PostSerializer(serializers.ModelSerializer):
     owner = serializers.ReadOnlyField(source='owner.username')
     is_owner = serializers.SerializerMethodField()
     profile_id = serializers.ReadOnlyField(source='owner.profile.id')
     profile_image = serializers.ReadOnlyField(source='owner.profile.image.url')
+    like_id = serializers.SerializerMethodField()
 
     # to validate a field in a model use the rest framework’s field level validation methods.
     # They’re called: validate_fieldName
@@ -33,10 +35,20 @@ class PostSerializer(serializers.ModelSerializer):
         request = self.context['request']
         return request.user == obj.owner
 
+    def get_like_id(self, obj):
+        user = self.context['request'].user
+        if user.is_authenticated:
+            like = Likes.objects.filter(
+                owner=user, post=obj
+            ).first()
+            return like.id if like else None
+        return None
+
     class Meta:
         model = Post
         fields = [
             'id', 'owner', 'is_owner', 'profile_id',
             'profile_image', 'created_at', 'updated_at',
-            'title', 'content', 'image', 'image_filter'
+            'title', 'content', 'image', 'image_filter', 
+            'like_id'
         ]
