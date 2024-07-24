f532ae8e828feb7089cadd92ed19b5665a65d439
Dajana Isbaner <dajanaisbaner@gmx.de>
2024-07-24 11:39:23 +0000

    Refactor profile views

--- profiles/views.py
+++ profiles/views.py
@@ -1,45 +1,57 @@
-from django.http import Http404
-from rest_framework import status
-from rest_framework.views import APIView
-from rest_framework.response import Response
+# from django.http import Http404
+# from rest_framework import status
+# from rest_framework.views import APIView
+# from rest_framework.response import Response
+# from .models import Profile
+# from .serializers import ProfileSerializer
+# from drf_api.permissions import IsOwnerOrReadOnly
+
+from rest_framework import generics, permissions
+from drf_api.permissions import IsOwnerOrReadOnly
 from .models import Profile
 from .serializers import ProfileSerializer
-from drf_api.permissions import IsOwnerOrReadOnly
 
-class ProfileList(APIView):
-    def get(self, request):
-        profiles = Profile.objects.all()
-        serializer = ProfileSerializer(profiles, many=True, context={'request':request})
-        return Response(serializer.data)
+class ProfileList(generics.ListAPIView):
+    serializer_class = ProfileSerializer
+    queryset = Profile.objects.all()
+
+    # def get(self, request):
+    #     profiles = Profile.objects.all()
+    #     serializer = ProfileSerializer(profiles, many=True, context={'request':request})
+    #     return Response(serializer.data)
 
 
-class ProfileDetail(APIView):
-    # If we explicitly set the serializer_class attribute, the rest framework
-    # will automatically render a form
+class ProfileDetail(generics.RetrieveUpdateAPIView):
     serializer_class = ProfileSerializer
     permission_classes = [IsOwnerOrReadOnly]
-    def get_object(self, pk):
-        try:
-            profile = Profile.objects.get(pk=pk)
-            self.check_object_permissions(self.request, profile)
-            return profile
-        except Profile.DoesNotExist:
-            raise Http404
+    queryset = Profile.objects.all()
 
-    def get(self, request, pk):
-        profile = self.get_object(pk)
-        serializer = ProfileSerializer(profile, context={'request':request})
-        return Response(serializer.data)
+    # # If we explicitly set the serializer_class attribute, the rest framework
+    # # will automatically render a form
+    # serializer_class = ProfileSerializer
+    # permission_classes = [IsOwnerOrReadOnly]
+    # def get_object(self, pk):
+    #     try:
+    #         profile = Profile.objects.get(pk=pk)
+    #         self.check_object_permissions(self.request, profile)
+    #         return profile
+    #     except Profile.DoesNotExist:
+    #         raise Http404
 
-    def put(self, request, pk):
-        profile = self.get_object(pk)
-        serializer = ProfileSerializer(profile, data=request.data, context={'request':request})
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data)
-        return Response(
-            serializer.errors, status=status.HTTP_400_BAD_REQUEST
-        )
+    # def get(self, request, pk):
+    #     profile = self.get_object(pk)
+    #     serializer = ProfileSerializer(profile, context={'request':request})
+    #     return Response(serializer.data)
+
+    # def put(self, request, pk):
+    #     profile = self.get_object(pk)
+    #     serializer = ProfileSerializer(profile, data=request.data, context={'request':request})
+    #     if serializer.is_valid():
+    #         serializer.save()
+    #         return Response(serializer.data)
+    #     return Response(
+    #         serializer.errors, status=status.HTTP_400_BAD_REQUEST
+    #     )
 
         
 
