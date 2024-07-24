c1665a6d5f670edf467ee5ecfb8e4fcf2c9c0d92
Dajana Isbaner <dajanaisbaner@gmx.de>
2024-07-24 12:31:44 +0000

    Add followers, following and posts count to profile

--- posts/serializers.py
+++ posts/serializers.py
@@ -38,7 +38,7 @@ class PostSerializer(serializers.ModelSerializer):
     def get_like_id(self, obj):
         user = self.context['request'].user
         if user.is_authenticated:
-            like = Likes.objects.filter(
+            like = Like.objects.filter(
                 owner=user, post=obj
             ).first()
             return like.id if like else None
--- profiles/views.py
+++ profiles/views.py
@@ -6,14 +6,30 @@
 # from .serializers import ProfileSerializer
 # from drf_api.permissions import IsOwnerOrReadOnly
 
-from rest_framework import generics, permissions
+from django.db.models import Count
+from rest_framework import generics, filters
 from drf_api.permissions import IsOwnerOrReadOnly
 from .models import Profile
 from .serializers import ProfileSerializer
 
 class ProfileList(generics.ListAPIView):
     serializer_class = ProfileSerializer
-    queryset = Profile.objects.all()
+    # annotate function allows to define extra fields to be added to the queryset
+    queryset = Profile.objects.annotate(
+        posts_count = Count('owner__post', distinct=True),
+        followers_count = Count('owner__followed', distinct=True),
+        following_count = Count('owner__following', distinct=True)
+    ).order_by('created_at')
+    filter_backends = [
+        filters.OrderingFilter
+    ]
+    ordering_fields = [
+        'posts_count',
+        'followers_count',
+        'following_count',
+        'owner__following__created_at',
+        'owner__followed__created_at',
+    ]
 
     # def get(self, request):
     #     profiles = Profile.objects.all()
@@ -24,8 +40,14 @@ class ProfileList(generics.ListAPIView):
 class ProfileDetail(generics.RetrieveUpdateAPIView):
     serializer_class = ProfileSerializer
     permission_classes = [IsOwnerOrReadOnly]
-    queryset = Profile.objects.all()
+    queryset = Profile.objects.annotate(
+    posts_count = Count('owner__post', distinct=True),
+    followers_count = Count('owner__followed', distinct=True),
+    following_count = Count('owner__following', distinct=True)
+    ).order_by('created_at')
 
+    # Before refactor to generic view: 
+    # class ProfileDetail(APIView)
     # # If we explicitly set the serializer_class attribute, the rest framework
     # # will automatically render a form
     # serializer_class = ProfileSerializer
