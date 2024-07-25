6fb4c18eed500fd8f032b36922ba9acfb74b9ff7
Dajana Isbaner <dajanaisbaner@gmx.de>
2024-07-25 12:22:33 +0000

    Prepare code for walkthrough project

--- drf_api/settings.py
+++ drf_api/settings.py
@@ -13,6 +13,7 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 from pathlib import Path
 import os
 import dj_database_url
+import re
 
 if os.path.exists("env.py"):
     import env
@@ -63,7 +64,7 @@ DEBUG = 'DEV' in os.environ
 
 ALLOWED_HOSTS = [
     '8000-queenisabaer-drfapi-yz18zq6vsal.ws-eu115.gitpod.io',
-    'drfapi-6c5a49626360.herokuapp.com'
+    os.environ.get('ALLOWED_HOST'),
 ]
 
 
@@ -115,9 +116,10 @@ if 'CLIENT_ORIGIN' in os.environ:
     CORS_ALLOWED_ORIGINS = [
         os.environ.get('CLIENT_ORIGIN')
      ]
-else:
+if 'CLIENT_ORIGIN_DEV' in os.environ:
+    extracted_url = re.match(r'^.+-', os.environ.get('CLIENT_ORIGIN_DEV', ''), re.IGNORECASE).group(0)
     CORS_ALLOWED_ORIGIN_REGEXES = [
-        r"^https://.*\.gitpod\.io$",
+        rf"{extracted_url}(eu|us)\d+\w\.gitpod\.io$",
     ]
 
 CORS_ALLOW_CREDENTIALS = True
