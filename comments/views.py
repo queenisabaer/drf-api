54e86c74201c3b8041706d40cf032f3f0c0337e1
Dajana Isbaner <dajanaisbaner@gmx.de>
2024-07-25 08:09:03 +0000

    Add comment filters using django-filter

--- comments/views.py
+++ comments/views.py
@@ -1,5 +1,6 @@
 from rest_framework import generics, permissions
 from drf_api.permissions import IsOwnerOrReadOnly
+from django_filters.rest_framework import DjangoFilterBackend
 from .models import Comment
 from .serializers import CommentSerializer, CommentDetailSerializer
 
@@ -9,6 +10,13 @@ class CommentList(generics.ListCreateAPIView):
     # Instead of specifying only the model to use, in DRF we set the queryset attribute
     # This way, it is possible to filter out some of the model instances.(e.g. sensible data)
     queryset = Comment.objects.all()
+
+    filter_backends = [
+        DjangoFilterBackend,
+    ]
+    filterset_fields = [
+        'post',
+    ]
 
     def perform_create(self, serializers):
         serializers.save (owner=self.request.user)
